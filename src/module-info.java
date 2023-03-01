a6be58421773fc22eb4e8f9723ddedc76dea157e
Rithin <rithinkumar9329@gmail.com>
2023-03-01 12:38:50 +0530

    1st comit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author niveus
+ *
+ */
+module tettttt {
+}
---
+++ src/tettttt/testdtdt.java
@@ -0,0 +1,10 @@
+package tettttt;
+
+public class testdtdt {
+
+	public static void main(String[] args) {
+		System.out.println("testinkkkg");
+
+	}
+
+}
